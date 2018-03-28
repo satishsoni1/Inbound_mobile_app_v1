dd954bce6bc43a563012fa8fb1cb46614896ceff
satishsoni1 <satish.soni@globalspace.in>
2018-03-28 07:36:34 +0530

    Initial commit

---
+++ www/js/config.js
@@ -0,0 +1,8 @@
+/*Kartero App Configuration*/
+
+var krms_driver_config ={		
+	'ApiUrl':"http://ec2-13-126-171-124.ap-south-1.compute.amazonaws.com/Inbound_Project/admin/api",		
+	'DialogDefaultTitle':"Inbound",	
+	'PushProjectID':"281792327024",	
+	'APIHasKey':""
+};
